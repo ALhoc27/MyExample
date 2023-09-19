79eddb32fd0dea862c10dbb02f48f446bc388ec9
Бабицкий Александр Игоревич <alexbbtsk@gmail.com>
2023-09-19 19:23:51 +0500

    first commit

--- src/index/IndexTest.java
+++ src/index/IndexTest.java
@@ -12,6 +12,6 @@ class IndexTest {
         int[] inArray = {-20, -17, -15, -10, -3, 0, 1, 3, 5, 7};
         int[] result = Index.index(inArray);
         int[] expected = {0, 1, 9, 9, 25, 49, 100, 225, 289, 400};
-        //assertThat(Arrays.equals(result, expected)).isTrue();
+        assertThat(Arrays.equals(result, expected)).isTrue();
     }
 }
